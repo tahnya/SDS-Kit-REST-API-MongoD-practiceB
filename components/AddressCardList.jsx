aca4c5c52d8d9a70ad1ae55695c6d45d45030215
tahnya <ntahnya@LAPTOP-12GHLT27>
2026-04-04 00:22:30 +0800

    Add backend

--- components/AddressCardList.jsx
+++ components/AddressCardList.jsx
@@ -1,17 +1,29 @@
 'use client'
 
 import AddressCard from './AddressCard'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const addresses = [
-  {
-    id: 1,
-    title: 'Home',
-    address: '505 Independence Dr.',
-  },
-]
+
 
 // render list of AddressCard components
 const AddressCardList = () => {
+  const [addresses, setAddresses] = useState([])
+  useEffect(() => {
+    const fetchAddresses = async () => {
+      try {
+        // make a GET request to backend server via axios
+        const response = await
+        axios.get('http://localhost:8080/api/addresses')
+        // update the addresses state with the retrieved data
+        setAddresses(response.data)
+      } catch (error) {
+        console.error('Error fetching addresses:', error)
+      }
+    }
+    fetchAddresses()
+  }, [addresses])
+
   return (
     <div className="flex-1 max-w-3xl">
       {addresses.map((address) => (
@@ -22,3 +34,5 @@ const AddressCardList = () => {
 }
 
 export default AddressCardList
+
+
--- components/InputButton.jsx
+++ components/InputButton.jsx
@@ -1,14 +1,63 @@
 'use client'
 
 import { Plus } from 'lucide-react'
+import { useState } from 'react'
+import axios from 'axios'
 
 const InputButton = () => {
+  const [open, setOpen] = useState(false)
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+
+  const onSubmit = async (data) => {
+    try {
+      await axios.post('http://localhost:8080/api/addresses', data)
+      setOpen(false)
+    } catch (error) {
+      console.error('Error creating address:', error)
+    }
+  }
+
+  const InputBox = () => {
+    return (
+      <div className="flex flex-col bg-gray-100 p-4 rounded-lg">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            onSubmit({ name, description })
+          }}
+        >
+          <label>name:
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>description:
+            <input
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </label>
+          <input type="submit" />
+        </form>
+      </div>
+      
+    )
+
+  }
+
+
   return (
     <div className="flex bg-red-100 justify-end px-4 py-4 rounded-b-lg">
       <button
         className="flex items-center text-white transition ease-in-out delay-50 bg-gray-900 hover:scale-105 hover:bg-gray-800 duration-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
         onClick={() => {
-          console.log('clicked')
+          setOpen((prev) => !prev)
+          
+
         }}
       >
         <Plus className="pr-2" /> Add address
